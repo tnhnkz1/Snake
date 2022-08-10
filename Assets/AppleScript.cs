6c1b6b17b5a2815cfbfaaf6c5eb787d37af2def7
Tunahan İkiz <tuna.han.ikiz@hotmail.com>
2022-08-10 17:38:36 +0300

    Snake-Game V1

---
+++ Assets/AppleScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AppleScript : MonoBehaviour
+{
+   
+    
+    
+    public TMPro.TextMeshProUGUI Score_txt;
+    int Score = 0;
+
+    HeadScript Create_Tail;
+
+    
+
+    private void Start()
+    {
+        Create_Tail = GameObject.Find("Head").GetComponent<HeadScript>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Head")
+        {
+            Score += 10;
+            Score_txt.text = "SCORE " + Score;
+
+            change_coordinate();
+
+            Create_Tail.Increment_Tail();
+        }
+
+        if (other.gameObject.tag == "Tail")
+        {
+            change_coordinate();
+        }
+       
+    }
+
+    void change_coordinate()
+    {
+        float x_value = Random.Range(1.75f, -4.1f);
+        float z_value = Random.Range(18.99f, 24.784f);
+        
+
+        transform.position = new Vector3(x_value, 2.18f, z_value);
+    }
+
+   
+}
---
+++ Assets/HeadScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class HeadScript : MonoBehaviour
+{
+    public GameObject Tail;
+    List<GameObject> Tails;
+
+    Vector3 old_coordinate;
+    GameObject old_Tail;
+
+    float speed = 20.0f;
+
+    public GameObject Play_Again;
+
+    
+
+    void Start()
+    {
+        Tails = new List<GameObject>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject new_Tail = Instantiate(Tail, transform.position, Quaternion.identity);
+            Tails.Add(new_Tail);
+        }
+
+        InvokeRepeating("Move", 0.0f, 0.1f);
+    }
+
+    public void Increment_Tail()
+    {
+        GameObject new_Tail = Instantiate(Tail, transform.position, Quaternion.identity);
+        Tails.Add(new_Tail);
+    }
+
+    void Move()
+    {
+        old_coordinate = transform.position;
+
+        transform.Translate(0, 0, speed * Time.deltaTime);
+
+        Tail_Follow();
+    }
+    
+    void Tail_Follow()
+    {
+        Tails[0].transform.position = old_coordinate;
+        old_Tail = Tails[0];
+        Tails.RemoveAt(0);
+        Tails.Add(old_Tail);
+    }
+
+    public void rotate(float angle)
+    {
+        transform.Rotate(0, angle, 0);
+
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Play_Again.SetActive(true);
+            Time.timeScale = 0.0f;
+       }
+
+        if (other.gameObject.tag == "Tail")
+        {
+            Play_Again.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    public void Reload()
+    {
+        Time.timeScale = 1.0f;
+
+        SceneManager.LoadScene("Snake/Scene/Snake");
+    }
+    
+
+}
+  
